defec74770af0680bfccc9ff8cb2ddf4353d9360
Md Soyaib <mdsoyaib2025@gmail.com>
2021-11-29 23:09:13 +0600

    test commit

--- core/models.py
+++ core/models.py
@@ -23,13 +23,3 @@ class Product(models.Model):
     quantity = models.IntegerField(null=True, blank=True)
     unit_type = models.CharField(max_length=20, choices=unit, default="Select Quantity")
     photo = models.ImageField(upload_to='uploads/product', null=True, blank=True)
-
-
-    @staticmethod
-    def get_all_products_by_category_id(category_id):
-        if category_id:
-            return Product.objects.filter(category=category_id)
-        else:
-            return Product.objects.all()
-
-
